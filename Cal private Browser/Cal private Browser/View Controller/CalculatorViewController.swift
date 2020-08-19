7ef50c87e946be3cdfd671d70c76819dc9b04ac8
fritzgt <tuanis@live.com>
2020-08-19 14:33:36 +0300

    completed function to accumulate numbers

--- Cal private Browser/Cal private Browser/View Controller/CalculatorViewController.swift
+++ Cal private Browser/Cal private Browser/View Controller/CalculatorViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
-    //MARK: - Properties
+    //MARK: - Outlets
     @IBOutlet weak var displayLabel: UILabel!
     
-    //MARK: - Outlets
-    
+    //MARK: - Properties
+    private var isFinishTypingNumber: Bool = true
 
     //MARK: - View Lifecyle
     
@@ -27,11 +27,23 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func calcButtonPressed(_ sender: UIButton){
         
+        //Set to true to clear the textfield the next time user start typing
+        isFinishTypingNumber = true
     }
     
     
     @IBAction func numButtonPressed(_ sender: UIButton){
-      
+        if let numVal = sender.currentTitle {
+            
+            if isFinishTypingNumber{
+                displayLabel.text = numVal
+                //Set to signal that we just start typing so the following numbers will be appended to the string in the else statement below
+                isFinishTypingNumber = false
+            }else{
+                displayLabel.text?.append(numVal)
+            }
+        }
+        
     }
     
 
