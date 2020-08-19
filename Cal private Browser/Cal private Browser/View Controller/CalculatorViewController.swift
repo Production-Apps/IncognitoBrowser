f903c4f6e2d2c5b7fd04ea79a1aa8643f870914c
fritzgt <tuanis@live.com>
2020-08-19 23:04:19 +0300

    refactor getter

--- Cal private Browser/Cal private Browser/View Controller/CalculatorViewController.swift
+++ Cal private Browser/Cal private Browser/View Controller/CalculatorViewController.swift
@@ -15,6 +15,13 @@ class CalculatorViewController: UIViewController {
     
     //MARK: - Properties
     private var isFinishTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get{
+            guard let doubleValue = Double(displayLabel.text!) else { return 0.0 }
+            return doubleValue
+        }
+    }
 
     //MARK: - View Lifecyle
     
@@ -29,19 +36,14 @@ class CalculatorViewController: UIViewController {
         //Set to true to clear the textfield the next time user start typing
         isFinishTypingNumber = true
         
-        //Can be force unwrap because there will always be a num
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert number into Double.")
-        }
-        
         if let calcMethod = sender.currentTitle {
             switch calcMethod {
             case "+/-":
-                displayLabel.text = String(number * -1)
+                displayLabel.text = String(displayValue * -1)
             case "AC":
                 displayLabel.text = "0"
             case "%":
-                displayLabel.text = String(number * 0.01)
+                displayLabel.text = String(displayValue * 0.01)
             case "÷":
             //Do something
                 displayLabel.text = "0"
@@ -73,14 +75,10 @@ class CalculatorViewController: UIViewController {
                 //prevent more than one decimal to be type by user
                 if numVal == "."{
                     
-                    guard let currDisplayVal = Double(displayLabel.text!) else {
-                        fatalError("Cannot convert current value into double")
-                    }
-                    
                     //Check if the rounded value is equal to the current value
                     //EX: on label 8.2 --- rounded 8 = isInt is False
                     //EX: on label 8 --- rounded 8 = isInt is True
-                    let isInt = floor(currDisplayVal) == currDisplayVal
+                    let isInt = floor(displayValue) == displayValue
                     
                     //If it is Int then we allow a decimal point
                     //If is not then it means that we have a decimal value already so we cannot allow more decimal points to be enter
