0a6aaee37f7550aa2006054e3b417c4cf6cd218d
fritzgt <tuanis@live.com>
2020-08-19 16:17:02 +0300

    handle extra decimal point and operators

--- Cal private Browser/Cal private Browser/View Controller/CalculatorViewController.swift
+++ Cal private Browser/Cal private Browser/View Controller/CalculatorViewController.swift
@@ -26,9 +26,39 @@ class CalculatorViewController: UIViewController {
     //MARK: - Actions
     
     @IBAction func calcButtonPressed(_ sender: UIButton){
-        
         //Set to true to clear the textfield the next time user start typing
         isFinishTypingNumber = true
+        
+        //Can be force unwrap because there will always be a num
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert number into Double.")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            switch calcMethod {
+            case "+/-":
+                displayLabel.text = String(number * -1)
+            case "AC":
+                displayLabel.text = "0"
+            case "%":
+                displayLabel.text = String(number * 0.01)
+            case "÷":
+            //Do something
+                displayLabel.text = "0"
+            case "×":
+            //Do something
+                displayLabel.text = "0"
+            case "+":
+            //Do something
+                displayLabel.text = "0"
+            case "=":
+            //Do something
+                displayLabel.text = "0"
+            default:
+                print("Case not found")
+            }
+        }
+        
     }
     
     
@@ -40,6 +70,27 @@ class CalculatorViewController: UIViewController {
                 //Set to signal that we just start typing so the following numbers will be appended to the string in the else statement below
                 isFinishTypingNumber = false
             }else{
+                //prevent more than one decimal to be type by user
+                if numVal == "."{
+                    
+                    guard let currDisplayVal = Double(displayLabel.text!) else {
+                        fatalError("Cannot convert current value into double")
+                    }
+                    
+                    //Check if the rounded value is equal to the current value
+                    //EX: on label 8.2 --- rounded 8 = isInt is False
+                    //EX: on label 8 --- rounded 8 = isInt is True
+                    let isInt = floor(currDisplayVal) == currDisplayVal
+                    
+                    //If it is Int then we allow a decimal point
+                    //If is not then it means that we have a decimal value already so we cannot allow more decimal points to be enter
+                    if !isInt{
+                        //By returning we wont allow more decimal points to be enter
+                        return
+                    }
+                    
+                }
+                
                 displayLabel.text?.append(numVal)
             }
         }
