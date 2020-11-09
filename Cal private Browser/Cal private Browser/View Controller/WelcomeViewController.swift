00fcc88171fb3ea6049724e74500b6badaab2d89
fritzgt <fritzgt@icloud.com>
2020-11-09 07:46:52 -0600

    create onboarding ui

--- Cal private Browser/Cal private Browser/View Controller/WelcomeViewController.swift
+++ Cal private Browser/Cal private Browser/View Controller/WelcomeViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    //MARK: - Properties
+    private var titles = ["Create a passcode","Enter passcode", "Swipe right to access"]
+    
     //MARK: - Outlets
     @IBOutlet var holderView: UIView! 
 
@@ -35,7 +38,38 @@ class WelcomeViewController: UIViewController {
             scrollView.addSubview(pageView)
             
             //Setup title, image and button
+            let label = UILabel(frame: CGRect(x: 10, y: 10, width: pageView.frame.width - 20, height: 120))
+            let imageView = UIImageView(frame: CGRect(x: 10, y: 140, width: pageView.frame.width - 20, height: pageView.frame.height - 205))
+            let button = UIButton(frame: CGRect(x: 10, y: pageView.frame.height-60, width: pageView.frame.width - 20, height: 50))
+            
+            //Configure label
+            label.textAlignment = .center
+            label.font = UIFont(name: "Helvetica-Bold", size: 32)
+            label.text = titles[x]
+            pageView.addSubview(label)
+            
+            //Configure image view
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = UIImage(named: "welcome\(x)")
+            pageView.addSubview(imageView)
+            
+            //Configure button
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = .black
+            
+            button.setTitle("Get started", for: .normal)
+            
+            if x == 1{
+                button.setTitle("Continue", for: .normal)
+            }
+            button.addTarget(self, action: #selector(didTapButton(_:)), for: .touchUpInside)
+            pageView.addSubview(button)
+            
         }
+        
+    }
+    
+    @objc private func didTapButton(_ button: UIButton) {
         
     }
     
